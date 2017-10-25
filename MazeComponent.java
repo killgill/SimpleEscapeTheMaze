643b700e8989c24d49d14e70d85185b36d30d936
Karan Singh Gill <gill@cooper.edu>
2017-10-25 16:45:40 -0700

    hopefully almost done

--- MazeComponent.java
+++ MazeComponent.java
@@ -11,6 +11,8 @@ import java.awt.Color;
 import java.util.Scanner;
 import java.io.Reader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
    MazeComponent class
@@ -24,15 +26,19 @@ public class MazeComponent extends JComponent
    private static final int START_Y = 10;
    private static final int BOX_WIDTH = 20;  // width and height of one maze "location"
    private static final int BOX_HEIGHT = 20;
+   private static final int PATH_INSET = 4;
+   private static final int PATH_THICKNESS = 2;
    private static final int INSET = 2; //how much smaller on each side to make entry/exit 
    private int rows;
    private int cols;
    private boolean[][] mazeData;
    private MazeCoord entryLoc;
    private MazeCoord exitLoc;
+   private LinkedList<MazeCoord> path;
    private final Color BLACK = new Color(0,0,0);
    private final Color WHITE = new Color(255,255,255);
    private final Color YELLOW = new Color(255,255,0);
+   private final Color BLUE = new Color(0,0,255);
    private final Color GREEN = new Color(0,255,0);
 
    
@@ -52,6 +58,7 @@ public class MazeComponent extends JComponent
       }
       entryLoc = maze.getEntryLoc();
       exitLoc = maze.getExitLoc();
+      path = maze.getPath();
 
    }
 
@@ -64,7 +71,48 @@ public class MazeComponent extends JComponent
    public void paintComponent(Graphics g)
    {
       Graphics2D g2 = (Graphics2D) g;
+      paintMaze(g2);
+      entryExit(g2);
+      drawPath(g2);
 
+   }
+
+   private void drawPath(Graphics2D g2) {
+      ListIterator<MazeCoord> it = path.listIterator();
+      MazeCoord coord1;
+      MazeCoord coord2;
+      g2.setColor(BLUE);
+      for (int i = 0; i < path.size() -1; i++) {
+	 coord1 = it.next();
+	 coord2 = it.next();
+	 int row1 = coord1.getRow();
+	 int row2 = coord2.getRow();
+	 int col1 = coord1.getCol();
+	 int col2 = coord2.getCol();
+	 if (row1 != row2) {
+	    Rectangle r = new Rectangle(START_X + col1*BOX_WIDTH + PATH_INSET, START_Y + Math.min(row1, row2)*BOX_HEIGHT + PATH_INSET, PATH_THICKNESS, BOX_HEIGHT + PATH_THICKNESS);
+	    g2.fill(r);
+	 }
+	 else if (col1 != col2) {
+	    Rectangle r = new Rectangle(START_X + Math.min(col1, col2)*BOX_WIDTH + PATH_INSET, START_Y + row1*BOX_HEIGHT + PATH_INSET, BOX_WIDTH + PATH_THICKNESS, PATH_THICKNESS);
+	    g2.fill(r);
+	 }
+	 it.previous();
+      }
+   }
+
+   private void entryExit(Graphics2D g2) {
+      g2.setColor(YELLOW);
+      Rectangle startLoc = new Rectangle(START_X + entryLoc.getCol()*BOX_WIDTH + INSET, START_Y + entryLoc.getRow()*BOX_HEIGHT + INSET, BOX_WIDTH - 2*INSET, BOX_HEIGHT - 2*INSET);
+      
+      g2.fill(startLoc);
+      g2.setColor(GREEN);
+      Rectangle endLoc = new Rectangle(START_X + exitLoc.getCol()*BOX_WIDTH + INSET, START_Y + exitLoc.getRow()*BOX_HEIGHT + INSET, BOX_WIDTH - 2*INSET, BOX_HEIGHT - 2*INSET);
+
+      g2.fill(endLoc);
+   }
+
+   private void paintMaze(Graphics2D g2) {
       Rectangle[][] mazeBoxes = new Rectangle[rows][cols];
       for (int i = 0; i < rows; i++) {
 	 for (int j = 0; j < cols; j++) {
@@ -81,16 +129,6 @@ public class MazeComponent extends JComponent
 	    mazeBoxes[i][j] = r;
 	 }
       }
-      g2.setColor(YELLOW);
-      Rectangle startLoc = new Rectangle(START_X + entryLoc.getCol()*BOX_WIDTH + INSET, START_Y + entryLoc.getRow()*BOX_HEIGHT + INSET, BOX_WIDTH - 2*INSET, BOX_HEIGHT - 2*INSET);
-      
-      g2.fill(startLoc);
-      g2.setColor(GREEN);
-      Rectangle endLoc = new Rectangle(START_X + exitLoc.getCol()*BOX_WIDTH + INSET, START_Y + exitLoc.getRow()*BOX_HEIGHT + INSET, BOX_WIDTH - 2*INSET, BOX_HEIGHT - 2*INSET);
-
-      g2.fill(endLoc);
    }
 }
-
-
 
